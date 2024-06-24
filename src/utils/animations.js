02d3d35b65862a287b26b7038aef96ff985223b1
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-06-25 02:55:26 +0530

    implemented how it works

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
-export const animateWithGSAP = (target, animationProps, scrollProps) => {
+export const animateWithGsap = (target, animationProps, scrollProps) => {
   gsap.to(target, {
     ...animationProps,
     scrollTrigger: {
